298216b51dbf258168430cd999c0dad57351db29
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-19 21:37:26 +0000

    Code changes to add storing and retrieving from MongoDB database

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,19 +9,18 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection("tweets").insertOne(newTweet, (err,result) => {
+        callback(err, result);
+      })
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection("tweets").find().toArray((err, tweets) => {
+        callback(err, tweets);
       });
     }
-
-  };
+  }
 }
+
+
