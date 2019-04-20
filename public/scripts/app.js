2c68e4c21084650a9be8be3ec90def8db0944d54
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-20 01:14:42 +0000

    Changes to confirm tweet character counter is reset after the tweet has been submitted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,7 +9,7 @@ function loadTweets () {
    .done(function (data){
     renderTweets(data);
    })
-}
+};
 
 function renderTweets(inputdata) {
   $('.tweets').empty();
@@ -17,24 +17,24 @@ function renderTweets(inputdata) {
     var $tweet = createTweetElement(item)
     $('.tweets').prepend($tweet);
   })
-}
+};
 
 function escape(str) {
   var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
 //The following code created the tweet container dynamically and returns the new tweet.
 
 function createTweetElement(data) {
-  let tweetImage = data.user.avatars.small;
-  let username = data.user.name;
-  let userHandle = data.user.handle;
-  let tweetBody = data.content.text;
+  let tweetImage      = data.user.avatars.small;
+  let username        = data.user.name;
+  let userHandle      = data.user.handle;
+  let tweetBody       = data.content.text;
   let tweetCreateDate = data.created_at;
-  let currentTime = Math.floor(Date.now());
-  let daysElapsed = Math.floor((currentTime - tweetCreateDate) / (1000*60*60*24));
+  let currentTime     = Math.floor(Date.now());
+  let daysElapsed     = Math.floor((currentTime - tweetCreateDate) / (1000*60*60*24));
 
   let HTMLToAppend = `<article class="tweet">
                       <header class="tweet-header">
@@ -55,30 +55,30 @@ function createTweetElement(data) {
                       <img class="icon" src="/images/love.png"></img>
                       </figcaption>
                       </footer>
-                      </article>`
+                      </article>`;
   return HTMLToAppend;
-}
+};
 
 $(document).ready(function() {
   $('#compose-button').on('click', function (event) {
     $('.new-tweet').toggle();
-  })
+  });
 
   loadTweets();
 //Following code submits a request to the server
 //It also checks for the validity of the tweet performs the POST only if
 //the tweet is valid and length is greater than 140 characters.
 
-  $(".submit-button").on('click', function(event) {
+  $('.submit-button').on('click', function(event) {
     event.preventDefault();
-    let tweet = $("textarea").val();
-    let tweetLength = $("textarea").val().length;
+    let tweet = $('textarea').val();
+    let tweetLength = $('textarea').val().length;
     if (!tweet) {
-        let $tweetError = `<p class="error-message">Tweet is empty</p>`
+        let $tweetError = `<p class='error-message'>Tweet is empty</p>`;
         $('.counter').append($tweetError);
         $('.error-message').toggle();
     } else if (tweetLength > 140) {
-        let $tweetTooLong = `<p class="error-message">Tweet is longer than 140</p>`
+        let $tweetTooLong = `<p class='error-message'>Tweet is longer than 140</p>`;
         $('.counter').append($tweetTooLong);
         $('.error-message').toggle();
     } else {
@@ -91,7 +91,8 @@ $(document).ready(function() {
         .done( function() {
           loadTweets();
         })
-        $('#tweet-text').val("");
+        $('#tweet-text').val('');
+        $('.counter').text('140');
 
     }
   })
